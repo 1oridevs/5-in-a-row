93ed48e4e739f7e25692569376f20c3ff8eb15fa
1oridevs <oridevs.offical@gmail.com>
2024-11-20 11:19:35 +0200

    updates to serve HTML

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ app.use(express.json());
 
 app.use('/api', gameRoutes);
 
+app.use(express.static('public')); // Serve static files
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
